6567f85242126bc4abda1c0f75b0de3beea99262
MyeongchanJin <jmcchan@naver.com>
2020-08-02 23:15:34 +0900

    for문 입력 제거

--- src/example/array/ArrayTest.java
+++ src/example/array/ArrayTest.java
@@ -6,10 +6,6 @@ public class ArrayTest {
         int[] myArray = new int[10];
 
         for (int i = 0; i < 10; i++) {
-            myArray[i] = i;
-        }
-
-        for (int i = 0; i < 10; i++) {
             System.out.println("index " + i + ": " + myArray[i]);
         }
     }

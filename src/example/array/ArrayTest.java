c26cf2965295ab87b94fa01aae49cf1f9b5f8471
MyeongchanJin <jmcchan@naver.com>
2020-08-07 20:14:30 +0900

    객체 리스트 부분을 객체 배열로 변경

--- src/example/array/ArrayTest.java
+++ src/example/array/ArrayTest.java
@@ -6,8 +6,8 @@ import java.util.stream.Collectors;
 
 // 기본 동작, static 함수로 추가
 // 요소는 자연수로 제한
-public class ArrayTest {
-    public static final int LENGTH = 10;
+public class ArrayTest{
+    private static final int LENGTH = 10;
 
     public static void main(String[] args) {
         // LENGTH 만큼의 길이를 가진 배열 할당(배열의 크기 = 길이)
@@ -110,7 +110,7 @@ public class ArrayTest {
             System.out.println("not equal");
         }
 
-        
+
         /** copy (불변 객체) */
         int[] source1d = {5, 10, 15, 20, 25};
         int[][] source2d = new int[][]{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
@@ -394,144 +394,63 @@ public class ArrayTest {
 
 
         /** object copy (가변 객체) */
-        List<MutableObject> sources = new ArrayList<>();
-        sources.add(new MutableObject(1));
-        sources.add(new MutableObject(2));
-        sources.add(new MutableObject(3));
-        sources.add(new MutableObject(4));
-
+        MutableObject[] sources = new MutableObject[]{
+                new MutableObject(1), new MutableObject(2),new MutableObject(3), new MutableObject(4)};
 
         /* reference(shallow copy) */
-        System.out.println("객체 리스트 - 참조");
+        System.out.println("객체 배열 - 참조");
         print(true, sources);
-        List<MutableObject> targetRef = sources;
+        MutableObject[] objTargetRef = sources;
 
-        targetRef.add(new MutableObject(50));
+        objTargetRef[3] = new MutableObject(100);
+
         System.out.println("복사본 변경 후(얕은 복사)");
         print(true, sources);
-        print(false, targetRef);
-        sources.remove(sources.size() - 1); // 추가했던 객체 제거
+        print(false, objTargetRef);
+        sources[3] = new MutableObject(4);
         System.out.println("--------------------------------------\n");
 
-
-        /* copy constructor */
-        System.out.println("객체 리스트 - 복사 생성자");
+        /* incomplete deep copy(clone()) */
+        System.out.println("객체 배열 clone() - 1차원 배열");
         print(true, sources);
-        List<MutableObject> targetConstructor = new ArrayList<>(sources); // 복사 생성자
+        MutableObject[] objTarget1dClone = sources.clone();
 
-        System.out.println("1. 복사본에 객체 추가 후(깊은 복사?)");
-        targetConstructor.add(new MutableObject(100));
+        System.out.println("복사본 객체 교체");
+        objTarget1dClone[3] = new MutableObject(100);
 
         print(true, sources);
-        print(false, targetConstructor);
+        print(false, objTarget1dClone);
+
+        System.out.println("복사본 객체의 값 교체(얕은 복사)");
+        objTarget1dClone[1].setX(200);
+
+        print(true, sources);
+        print(false, objTarget1dClone);
+        objTarget1dClone[1].setX(2);
         System.out.println("--------------------------------------\n");
 
-        System.out.println("2. 복사본 객체 변경 후(얕은 복사가 됨)");
-        targetConstructor.get(0).setX(50);
-
+        /* complete deep copy(direct copy) */
+        System.out.println("객체 배열 - 1차원 배열");
         print(true, sources);
-        print(false, targetConstructor);
+        MutableObject[] objTarget1dDirect = new MutableObject[sources.length];
 
-        sources.get(0).setX(1); // 원상복구
-        System.out.println("--------------------------------------\n");
-
-
-        /* Collections.copy() */
-        System.out.println("객체 리스트 - copy()");
-        print(true, sources);
-        List<MutableObject> targetCopy = new ArrayList<>();
-        targetCopy.add(new MutableObject(0)); // 더미 객체로 메모리 확보
-        targetCopy.add(new MutableObject(0));
-        targetCopy.add(new MutableObject(0));
-        targetCopy.add(new MutableObject(0));
-
-        // 메모리 용량이 미리 확보되어 있어야 해서 때문에 클론 생성 용도로 쓰기 힘들다
-        Collections.copy(targetCopy, sources);
-
-        System.out.println("1. 복사본에 객체 추가 후(깊은 복사?)");
-        targetCopy.add(new MutableObject(10));
-
-        print(true, sources);
-        print(false, targetCopy);
-        System.out.println("--------------------------------------\n");
-
-        System.out.println("2. 복사본 객체 변경 후(얕은 복사가 됨)");
-        targetCopy.get(1).setX(50);
-
-        print(true, sources);
-        print(false, targetCopy);
-
-        targetCopy.get(1).setX(2); // 원상복구
-        System.out.println("--------------------------------------\n");
-
-
-        /* Collections.addAll() */
-        System.out.println("객체 리스트 - addAll()");
-        print(true, sources);
-        List<MutableObject> targetAddAll = new ArrayList<>();
-        targetAddAll.addAll(sources);
-
-        System.out.println("1. 복사본에 객체 추가 후(깊은 복사?)");
-        targetAddAll.add(new MutableObject(300));
-
-        print(true, sources);
-        print(false, targetAddAll);
-        System.out.println("--------------------------------------\n");
-
-        System.out.println("2. 복사본 객체 변경 후(값 변경됨)");
-        targetAddAll.get(2).setX(20);
-
-        print(true, sources);
-        print(false, targetAddAll);
-
-        sources.get(2).setX(3); // 원상복구
-        System.out.println("--------------------------------------\n");
-
-
-        /* with stream */
-        System.out.println("객체 리스트 - stream() 이용)");
-        print(true, sources);
-        List<MutableObject> targetStream;
-        targetStream = sources.stream().collect(Collectors.toList());
-
-        System.out.println("1. 복사본에 객체 추가 후(깊은 복사?)");
-        targetStream.add(new MutableObject(500));
-
-        print(true, sources);
-        print(false, targetStream);
-        System.out.println("--------------------------------------\n");
-
-        System.out.println("2. 복사본 객체 변경 후(얕은 복사가 됨)");
-        targetStream.get(0).setX(50);
-
-        print(true, sources);
-        print(false, targetStream);
-
-        sources.get(0).setX(1); // 원상복구
-        System.out.println("--------------------------------------\n");
-
-
-        /* direct copy */
-        System.out.println("객체 리스트 - 직접 복사");
-        print(true, sources);
-        List<MutableObject> targetDirect = new ArrayList<>();
-        for (MutableObject source : sources) {
-            targetDirect.add(new MutableObject(source.x));
+        System.out.println("직접 추가");
+        for (int i = 0; i < sources.length; i++) {
+            objTarget1dDirect[i] = new MutableObject(sources[i].x);
         }
-        System.out.println("1. 복사본에 객체 추가 후(깊은 복사?)");
-        targetDirect.add(new MutableObject(50));
 
         print(true, sources);
-        print(false, targetDirect);
+        print(false, objTarget1dDirect);
 
-        System.out.println("2. 복사본 객체 변경 후(깊은 복사!)");
-        targetDirect.get(0).setX(100);
+        System.out.println("복사본 객체의 값 교체(깊은 복사)");
+        objTarget1dDirect[3].setX(500);
 
         print(true, sources);
-        print(false, targetDirect);
+        print(false, objTarget1dDirect);
         System.out.println("--------------------------------------\n");
+
     }
-    
+
     public static void print(Object T) {
         if (T instanceof int[]) {
             Integer[] newInteger1dArr = Arrays.stream((int[]) T).boxed().toArray(Integer[]::new);
@@ -548,14 +467,12 @@ public class ArrayTest {
             System.out.println(toCustomString((String[]) T));
         else if (T instanceof String[][])
             System.out.println(toCustomString((String[][]) T));
-        else if (T instanceof List<?>) {
-            if (((List<?>) T).get(0) instanceof MutableObject) {
-                int i = 0;
-                System.out.println();
-                for (MutableObject obj : (List<MutableObject>) T) {
-                    System.out.println("item " + i + ": " + obj.toString());
-                    i++;
-                }
+        else if (T instanceof MutableObject[]) {
+            int i = 0;
+            System.out.println();
+            for (MutableObject obj : (MutableObject[]) T) {
+                System.out.println("item " + i + ": " + obj.toString());
+                i++;
             }
         }
         else {
@@ -569,7 +486,7 @@ public class ArrayTest {
         print(T);
     }
 
-    public static <T> String toCustomString(T[][] array) {
+    private static <T> String toCustomString(T[][] array) {
         String arrayStr = "[\n";
         for (int i = 0; i < array.length; i++) {
             if (i == array.length -1)
@@ -581,7 +498,7 @@ public class ArrayTest {
         return arrayStr;
     }
 
-    public static <T> String toCustomString(T[] array) {
+    private static <T> String toCustomString(T[] array) {
         String arrayStr = "[";
         for (int i = 0; i < array.length; i++) {
             if (i == array.length - 1)

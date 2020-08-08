70da76a11f76a9440d797862dacf3044e41a2726
MyeongchanJin <jmcchan@naver.com>
2020-08-08 23:03:35 +0900

    ArrayList 설명 추가

--- src/example/arraylist/ArrayListTest.java
+++ src/example/arraylist/ArrayListTest.java
@@ -1,4 +1,11 @@
 package example.arraylist;
 
 public class ArrayListTest {
+    /** [rt.jar] java.util.ArrayList.class
+     * ArrayList
+     * 데이터를 *배열로 관리*하며, 순서에 변화가 일어나면 새로운 배열로 데이터를 *복사*하여 해결한다.
+     * 배열로 관리하기 때문에 배열의 인덱스가 갖는 장점을 그대로 가지고 있다.
+     * 순서에 변동이 생길 때 마다 새 배열을 만들어 복사하는 작업이 반복되므로
+     * 이런 작업이 빈번하다면 성능 저하가 생길 수 있다.
+     * */
 }
